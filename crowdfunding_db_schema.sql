b7ca72c5492ce56341e3b9c49b9775974f0483bb
Freemark0211 <128958764+Freemark0211@users.noreply.github.com>
2023-06-26 22:10:23 -0400

    new schema

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,8 +1,8 @@
-
-DROP TABLE IF EXISTS Subcategory;
-DROP TABLE IF EXISTS Category;
-DROP TABLE IF EXISTS Contacts;
-DROP TABLE IF EXISTS Crowdfunding;
+-- For Dropping Tables if needed
+--DROP TABLE IF EXISTS Subcategory;
+--DROP TABLE IF EXISTS Category;
+--DROP TABLE IF EXISTS Contacts;
+--DROP TABLE IF EXISTS Crowdfunding;
 
 
 CREATE TABLE Subcategory (
@@ -19,7 +19,7 @@ CREATE TABLE Contacts (
     "contact_id" int PRIMARY KEY,
     "first_name" varchar(40)   NOT NULL,
     "last_name" varchar(40)   NOT NULL,
-    "email" varchar(40)   NOT NULL
+    "email" varchar(100)   NOT NULL
 );
 
 CREATE TABLE Crowdfunding (
@@ -27,8 +27,8 @@ CREATE TABLE Crowdfunding (
     "contact_id" int   NOT NULL,
     "company_name" varchar(40)   NOT NULL,
     "description" varchar(100)   NOT NULL,
-    "goal" int   NOT NULL,
-    "pledged" int   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" float   NOT NULL,
     "outcome" varchar(40)   NOT NULL,
     "backers_count" int   NOT NULL,
     "country" varchar(40)   NOT NULL,
@@ -44,6 +44,6 @@ CREATE TABLE Crowdfunding (
 	FOREIGN KEY ("subcategory_id") REFERENCES Subcategory("subcategory_id")
 );
 
-SELECT * FROM Category;
+--SELECT * FROM Contacts;
 
 
