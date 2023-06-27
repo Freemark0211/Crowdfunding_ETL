b6d6c7807c47323c5ba80c17fe988af03f1917c1
Freemark0211 <128958764+Freemark0211@users.noreply.github.com>
2023-06-26 21:31:08 -0400

    table schema

--- crowdfunding-export.sql
+++ crowdfunding-export.sql
@@ -1,8 +1,13 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
+DROP TABLE IF EXISTS Subcategory;
+DROP TABLE IF EXISTS Category;
+DROP TABLE IF EXISTS Contacts;
+DROP TABLE IF EXISTS Crowdfunding;
 
-CREATE TABLE "Subcategory" (
+
+CREATE TABLE Subcategory (
     "subcategory_id" varchar(40)   NOT NULL,
     "subcategory" varchar(40)   NOT NULL,
     CONSTRAINT "pk_Subcategory" PRIMARY KEY (
@@ -10,7 +15,7 @@ CREATE TABLE "Subcategory" (
      )
 );
 
-CREATE TABLE "Category" (
+CREATE TABLE Category (
     "category_id" varchar(40)   NOT NULL,
     "category" varchar(40)   NOT NULL,
     CONSTRAINT "pk_Category" PRIMARY KEY (
@@ -18,7 +23,17 @@ CREATE TABLE "Category" (
      )
 );
 
-CREATE TABLE "Crowdfunding" (
+CREATE TABLE Contacts (
+    "contact_id" int   NOT NULL,
+    "first_name" varchar(40)   NOT NULL,
+    "last_name" varchar(40)   NOT NULL,
+    "email" varchar(40)   NOT NULL,
+    CONSTRAINT "pk_Contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE Crowdfunding (
     "cf_id" int   NOT NULL,
     "contact_id" int   NOT NULL,
     "company_name" varchar(40)   NOT NULL,
@@ -40,15 +55,7 @@ CREATE TABLE "Crowdfunding" (
      )
 );
 
-CREATE TABLE "Contacts" (
-    "contact_id" int   NOT NULL,
-    "first_name" varchar(40)   NOT NULL,
-    "last_name" varchar(40)   NOT NULL,
-    "email" varchar(40)   NOT NULL,
-    CONSTRAINT "pk_Contacts" PRIMARY KEY (
-        "contact_id"
-     )
-);
+
 
 ALTER TABLE "Crowdfunding" ADD CONSTRAINT "fk_Crowdfunding_contact_id" FOREIGN KEY("contact_id")
 REFERENCES "Contacts" ("contact_id");
